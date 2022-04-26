c070ce6d9cb013091d3ed962a579a6d6d689c5fe
Alex <ammeyer880@gmail.com>
2022-04-26 12:52:26 +1200

    added gui basics

---
+++ Documents/NetBeansProjects/PSCAssignment2/src/RPG/GUI/GUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RPG.GUI;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author alex
+ */
+public class GUI
+{
+    JFrame frame;
+}
